f5650bdb6a9a9727db47d96d93601bd945f3644f
Mostafa-Lakhloufi <mostafa.lakhloufi1@gmail.com>
2023-03-06 05:33:12 +0000

    sum

--- src/main/java/ArraySum.java
+++ src/main/java/ArraySum.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ArraySum {
     /**
      * This method should return the sum of all ints in an array - this is a very useful trick to know!
@@ -7,7 +9,13 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
-    }
+        int sum=0;
+        int i;
+         for(i = 0; i <arr.length; i++){
+            sum +=arr[i];
+         }
+         return sum;
+        }
+        
 }
+
